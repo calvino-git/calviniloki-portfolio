1caad10fec204a3b9fe4e5af5886acf3ef9e3ded
calvino <bbbebe661@yahoo.fr>
2024-12-31 15:23:54 +0100

    Update personal data

--- utils/data/contactsData.js
+++ utils/data/contactsData.js
@@ -1,10 +1,10 @@
 export const contactsData = {
-    email: 'abusaid7388@gmail.com',
-    phone: '+8801608797655',
-    address: 'Middle Badda, Dhaka, Bangladesh - 1212 ',
-    github: 'https://github.com/said7388',
+    email: 'ilokingakosso.calvin@gmail',
+    phone: '+242 06 425 0217',
+    address: 'Songolo, Pointe-Noire, République du Congo ',
+    github: 'https://github.com/calvino-git',
     facebook: 'https://www.facebook.com/abusaid.riyaz/',
-    linkedIn: 'https://www.linkedin.com/in/abu-said-bd/',
+    linkedIn: 'https://www.linkedin.com/in/calviniloki/',
     twitter: 'https://twitter.com/said7388',
     stackOverflow: 'https://stackoverflow.com/users/16840768/abu-said',
     devUsername: "calviniloki"
