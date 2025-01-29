e677123fba71cf6d6be5d5396c325e2a3a05b0b9
Calvin ILOKI <bbbebe661@yahoo.fr>
2025-01-29 18:13:59 +0100

    Update contactsData.js

--- utils/data/contactsData.js
+++ utils/data/contactsData.js
@@ -1,11 +1,9 @@
 export const contactsData = {
-    email: 'ilokingakosso.calvin@gmail',
+    email: 'ilokingakosso.calvin@gmail.com',
     phone: '+242 06 425 0217',
     address: 'Songolo, Pointe-Noire, République du Congo ',
     github: 'https://github.com/calvino-git',
     facebook: 'https://www.facebook.com/abusaid.riyaz/',
     linkedIn: 'https://www.linkedin.com/in/calviniloki/',
-    twitter: 'https://twitter.com/said7388',
-    stackOverflow: 'https://stackoverflow.com/users/16840768/abu-said',
     devUsername: "calviniloki"
 }
